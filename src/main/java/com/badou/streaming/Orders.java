1b79f13a53893e43607be1dac1f7a2ce60082017
kismet2399 <kismet2399@foxmail.com>
2018-11-25 09:15:16 +0800

    ddd

---
+++ src/main/java/com/badou/streaming/Orders.java
@@ -0,0 +1,22 @@
+package com.badou.streaming;
+
+public class Orders {
+    private String order_id;
+    private String user_id;
+
+    public String getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(String order_id) {
+        this.order_id = order_id;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+}
