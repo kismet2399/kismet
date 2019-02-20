5286606f8e5b70ca20747d7ae94af3c8c7c2bc53
zuozhuan <827446572@qq.com>
2019-02-20 15:34:45 +0800

    同步

---
+++ src/main/java/com/badou/HdfsFile2Hive.scala
@@ -0,0 +1,85 @@
+package com.badou
+
+import org.apache.spark.sql.{Row, SaveMode, SparkSession}
+import java.net.URI
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.fs.FileStatus
+import org.apache.hadoop.fs.FileUtil
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
+
+
+object HdfsFile2Hive {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("test")
+      .master("local[2]")
+      .getOrCreate()
+
+    val testRdd = spark.sparkContext.textFile("C:\\study\\projects\\python\\bayes\\raw_data")
+    testRdd.flatMap(_.split(" ").map((_, 1))).reduceByKey(_ + _)
+      .sortBy(_._2, ascending = false).take(20)
+      .foreach(println)
+
+    val path = "hdfs://master:9000/data/raw_data"
+
+    // 读取目录下所有的文件名存储在paths中
+    def getHdfs(path: String) = {
+      val conf = new Configuration()
+      FileSystem.get(URI.create(path), conf)
+    }
+
+    def getFilesAndDirs(path: String): Array[Path] = {
+      val fs = getHdfs(path).listStatus(new Path(path))
+      FileUtil.stat2Paths(fs)
+    }
+
+    val sc = spark.sparkContext
+    var paths = ListBuffer[String]()
+
+    def listAllFiles(path: String) {
+      val hdfs = getHdfs(path)
+      val listPath = getFilesAndDirs(path)
+      listPath.foreach(path => {
+        if (hdfs.getFileStatus(path).isFile())
+          paths.append(path.toString)
+        else {
+          listAllFiles(path.toString())
+        }
+      })
+    }
+
+    //将读取文件的标题改为lable
+    val matchLable = (filePath: String) => filePath match {
+      case filePath if filePath.contains("business") => "business"
+      case filePath if filePath.contains("yule") => "yule"
+      case filePath if filePath.contains("it") => "it"
+      case filePath if filePath.contains("sports") => "sports"
+      case filePath if filePath.contains("auto") => "auto"
+    }
+    //    val tem = paths.map(path => (path, matchLable(path)))
+    //获取(context,lable,segs)形式的ListBuffer
+    val listData = paths.map(filePath => (sc.textFile(filePath).collect().mkString("").replace(" ", ""),
+      matchLable(filePath), sc.textFile(filePath).collect().mkString("")))
+    // 设置临时dataFream结构
+    val schemaString = "sentence label segs"
+    val fields = schemaString.split(" ")
+      .map(fieldName => StructField(fieldName, StringType, nullable = true))
+    val schema = StructType(fields)
+    import spark.implicits._
+    val input = sc.parallelize(listData)
+    //要将rdd转换成RowRdd才能使用spark.createDataFrame(rowRdd,schema)多参数的方法
+    val rowRdd = input.map(x => Row(x._1, x._2, x._3))
+    val result = spark.createDataFrame(rowRdd,schema)
+    result.createOrReplaceTempView("temBlock")
+    //插入到数据库
+    spark.sql("select * from temBlock").write.mode(SaveMode.Overwrite).saveAsTable("news_noseg")
+
+  }
+
+}
